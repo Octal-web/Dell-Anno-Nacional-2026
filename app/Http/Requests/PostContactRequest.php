42167f2b1d93ed36c8af47283a631671e8ae2f37
RafaLaste <rafinhalaste@hotmail.com>
2026-04-29 10:42:29 -0300

    refactor: tradução mensagens Contato

--- app/Http/Requests/PostContactRequest.php
+++ app/Http/Requests/PostContactRequest.php
@@ -40,26 +40,16 @@ class PostContactRequest extends FormRequest
     public function messages()
     {
         return [
-            // 'nome.required' => 'Por favor, insira seu nome.',
-            // 'email.required' => 'Por favor, insira seu e-mail.',
-            // 'email.email' => 'Por favor, insira um e-mail válido.',
-            // 'telefone.required' => 'Por favor, insira seu telefone.',
-            // 'telefone.celular_com_ddd' => 'Por favor, informe um telefone válido.',
-            // 'estado_id.required' => 'Por favor, informe o seu estado.',
-            // 'cidade_id.required' => 'Por favor, informe a sua cidade.',
-            // 'mensagem.required'  => 'Por favor, informe a sua mensagem.',
-            // 'politica.required' => 'Para continuar, você deve concordar com a LGPD.',
-            // 'politica.accepted' => 'Para continuar, você deve concordar com a LGPD.',
-            'nome.required' => 'Please enter your name.',
-            'email.required' => 'Please enter your email.',
-            'email.email' => 'Please enter a valid email address.',
-            'telefone.required' => 'Please enter your phone number.',
-            'telefone.celular_com_ddd' => 'Please provide a valid phone number with area code.',
-            'estado_id.required' => 'Please select your state.',
-            'cidade_id.required' => 'Please select your city.',
-            'mensagem.required'  => 'Please enter your message.',
-            'politica.required' => 'To continue, you must agree with the privacy and cookies policies.',
-            'politica.accepted' => 'To continue, you must agree with the privacy and cookies policies.',
+            'nome.required' => 'Por favor, insira seu nome.',
+            'email.required' => 'Por favor, insira seu e-mail.',
+            'email.email' => 'Por favor, insira um e-mail válido.',
+            'telefone.required' => 'Por favor, insira seu telefone.',
+            'telefone.celular_com_ddd' => 'Por favor, informe um telefone válido.',
+            'estado_id.required' => 'Por favor, informe o seu estado.',
+            'cidade_id.required' => 'Por favor, informe a sua cidade.',
+            'mensagem.required'  => 'Por favor, informe a sua mensagem.',
+            'politica.required' => 'Para continuar, você deve concordar com a LGPD.',
+            'politica.accepted' => 'Para continuar, você deve concordar com a LGPD.',
         ];
     }
 }
