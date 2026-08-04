90b337b762ef96e4c5192c5c62d709731ad593dc
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-08-04 18:00:04 -0300

    refactor: altera a tradução da mensagem de fim dos resultados para pt

--- resources/js/Pages/Showrooms.jsx
+++ resources/js/Pages/Showrooms.jsx
@@ -16,7 +16,7 @@ const Page = () => {
 
             <ShowroomsLoadMore
                 initialData={initialShowrooms}
-                endMessage="end of results"
+                endMessage="Fim dos resultados"
                 rootMargin="200px"
             >
                 {({ data, loading, hasMore }) => (
