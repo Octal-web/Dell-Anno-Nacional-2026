4789dc864f683c22ceec22b22744f8dc59a07a3e
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-08-04 15:59:46 -0300

    refactor: altera para conteúdos dinâmicos em InspirationLinks

--- resources/js/Components/InspirationLinks.jsx
+++ resources/js/Components/InspirationLinks.jsx
@@ -1,29 +1,50 @@
 import { Link } from "@inertiajs/react";
 
-export const InspirationLinks = () => {
+export const InspirationLinks = ({ content }) => {
     return (
         <section className="py-16 md:py-24 2xl:py-30">
             <div className="container max-w-large">
                 <div className="grid grid-cols-1 md:grid-cols-3 max-sm:gap-y-10 gap-x-8">
-                    <Link href={route('Showrooms.index')} className="group">
+                    <Link href={content[0].link} className="group">
                         <div className="overflow-hidden mb-4 md:mb-10">
-                            <img src="/site/img/inspiration-1.jpg" className="transition-all duration-500 group-hover:scale-110" alt="Showrooms" />
+                            <img
+                                src={content[0].imagem}
+                                className="transition-all duration-500 group-hover:scale-110"
+                                alt={content[0].titulo}
+                            />
                         </div>
-                        <h3 className="text-xl md:text-2xl 2xl:text-[30px] font-light text-center uppercase tracking-wide">// Showrooms</h3>
+                        <h3 className="text-xl md:text-2xl 2xl:text-[30px] font-light text-center uppercase tracking-wide">
+                            // {content[0].titulo}
+                        </h3>
                     </Link>
-                    
-                    <Link href={route('Lojas.Projetos.index')} className="group">
+
+                    <Link
+                        href={content[1].link}
+                        className="group"
+                    >
                         <div className="overflow-hidden mb-4 md:mb-10">
-                            <img src="/site/img/inspiration-2.jpg" className="transition-all duration-500 group-hover:scale-110" alt="Projetos" />
+                            <img
+                                src="/site/img/inspiration-2.jpg"
+                                className="transition-all duration-500 group-hover:scale-110"
+                                alt={content[1].titulo}
+                            />
                         </div>
-                        <h3 className="text-xl md:text-2xl 2xl:text-[30px] font-light text-center uppercase tracking-wide">// Projetos</h3>
+                        <h3 className="text-xl md:text-2xl 2xl:text-[30px] font-light text-center uppercase tracking-wide">
+                            // {content[1].titulo}
+                        </h3>
                     </Link>
-                    
-                    <Link href={route('Mostras.index')} className="group">
+
+                    <Link href={content[2].link} className="group">
                         <div className="overflow-hidden mb-4 md:mb-10">
-                            <img src="/site/img/inspiration-3.jpg" className="transition-all duration-500 group-hover:scale-110" alt="Mostras de Decoração" />
+                            <img
+                                src="/site/img/inspiration-3.jpg"
+                                className="transition-all duration-500 group-hover:scale-110"
+                                alt={content[2].titulo}
+                            />
                         </div>
-                        <h3 className="text-xl md:text-2xl 2xl:text-[30px] font-light text-center uppercase tracking-wide">// Mostras de Decoração</h3>
+                        <h3 className="text-xl md:text-2xl 2xl:text-[30px] font-light text-center uppercase tracking-wide">
+                            // {content[2].titulo}
+                        </h3>
                     </Link>
                 </div>
             </div>
