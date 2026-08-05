a78f24cc61b6727532647b0ae94af8f5a903cbf8
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-08-05 15:47:30 -0300

    feat: validações para banner, categoria e dados de compartilhamento em PostPostRequest

--- app/Http/Requests/Manager/PostPostRequest.php
+++ app/Http/Requests/Manager/PostPostRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Manager;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PostPostRequest extends FormRequest
 {
@@ -20,15 +21,25 @@ class PostPostRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules()
-    {  
+    {
         return [
             'titulo' => 'required',
             'previa' => 'required',
             'conteudo' => 'required',
-            'publicado'  => 'nullable|date_format:Y-m-d\TH:i',
+            'publicado' => [
+                'nullable',
+                Rule::anyOf([
+                    'date_format:Y-m-d\TH:i',
+                    'date_format:Y-m-d H:i',
+                ]),
+            ],
+            'post_categoria_id' => 'required|exists:posts_categorias,id',
             'img' => inertia()->getShared('action') == 'novo' ? 'required|image|mimes:png,jpg|max:2048' : 'nullable|image|mimes:png,jpg|max:2048',
+            'img_banner' => inertia()->getShared('action') == 'novo' ? 'required|image|mimes:png,jpg|max:4096' : 'nullable|image|mimes:png,jpg|max:4096',
             'titulo_pagina' => 'required',
             'descricao_pagina' => 'required',
+            'titulo_compartilhamento' => 'required',
+            'descricao_compartilhamento' => 'required',
         ];
     }
 
@@ -51,9 +62,13 @@ class PostPostRequest extends FormRequest
             'img_banner.required' => 'Por favor, selecione um banner.',
             'img_banner.image' => 'Por favor, selecione um banner válido.',
             'img_banner.mimes' => 'Os formatos de imagem válidos são: JPG e PNG.',
-            'img_banner.max' => 'Por favor, envie um arquivo menor que 2MB.',
+            'img_banner.max' => 'Por favor, envie um arquivo menor que 4MB.',
             'titulo_pagina.required' => 'Por favor, informe o título da página.',
             'descricao_pagina.required' => 'Por favor, informe a descrição da página.',
+            'titulo_compartilhamento.required' => 'Por favor, informe o título de compartilhamento.',
+            'descricao_compartilhamento.required' => 'Por favor, informe a descrição de compartilhamento.',
+            'post_categoria_id.required' => 'Por favor, selecione uma categoria.',
+            'post_categoria_id.exists' => 'Por favor, selecione uma categoria válida.',
         ];
     }
 }
