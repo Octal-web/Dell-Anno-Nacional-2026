069902a98e23e408a9ba1960761b1dd28c9527f8
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-08-05 13:11:51 -0300

    refactor: altera layout de BlogPostsLists com efeito de hover

--- resources/js/Components/BlogPostsList.jsx
+++ resources/js/Components/BlogPostsList.jsx
@@ -1,28 +1,44 @@
-import { Link } from '@inertiajs/react';
-import { Reveal } from './Reveal';
+import { Link } from "@inertiajs/react";
+import { Reveal } from "./Reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const BlogPostsList = ({ data }) => {
     return (
         <section className="pt-20 md:pt-24 2xl:pt-30 pb-20 sm:pb-24 md:pb-32 2xl:pb-44">
             <div className="container max-w-large">
-                <div className="grid grid-cols-2 md:grid-cols-3 gap-x-3 md:gap-x-8 gap-y-14 md:gap-y-20">
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
                     {data.map((item, index) => (
-                        <Reveal direction="bottom" scale={true} className="group flex flex-col" key={index}>
-                            <Link href={route('Blog.post', {slug: item.slug})} className="overflow-hidden aspect-[5/4] md:aspect-[13/9]">
-                                <img src={item.imagem} className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110" alt={item.titulo} />
-                            </Link>
+                        <Reveal
+                            direction="bottom"
+                            scale={true}
+                            className="group flex flex-col"
+                            key={index}
+                        >
+                            <Link
+                                href={route("Blog.post", { slug: item.slug })}
+                                className="overflow-hidden aspect-[4/5] md:aspect-[5/6] relative"
+                            >
+                                <img
+                                    src={item.imagem}
+                                    className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110"
+                                    alt={item.titulo}
+                                />
 
-                            <div className="font-secondary text-neutral-500 max-sm:text-xs max-md:text-sm font-light mt-2 md:mt-6">
-                                {item.data}{/*  | {item.categoria} */}
-                            </div>
-                            
-                            <Link href={route('Blog.post', {slug: item.slug})} className="block my-4 md:my-6 transition-all hover:opacity-70">
-                                <h3 className="text-lg sm:text-xl md:text-2xl 2xl:text-[26px] min-h-12 sm:min-h-16 font-light max-sm:leading-tight max-w-sm line-clamp-2 mb-auto">{item.titulo}</h3>
-                            </Link>
+                                <div className="block absolute inset-0 bg-black opacity-0 transition-all duration-300 group-hover:opacity-50" />
 
-                            <p className="font-secondary max-sm:text-sm font-light sm:tracking-wide line-clamp-4 sm:line-clamp-5 max-w-md mb-5 md:mb-8 2xl:mb-10">{item.previa}</p>
-                                
-                            <Link href={route('Blog.post', {slug: item.slug})} className="mt-auto mr-auto border border-neutral-800 bg-white font-light text-center uppercase py-1.5 px-3 md:p-2 sm:min-w-44 transition-all hover:bg-black hover:text-white">Leia mais</Link>
+                                <div className=" absolute inset-0 opacity-0 transition-all duration-300 group-hover:opacity-100 z-10 flex items-end">
+                                    <div className="hidden absolute inset-0 lg:flex items-center justify-center">
+                                        <div className="bg-white/80 rounded-full px-4 py-3 shadow-lg">
+                                            <FontAwesomeIcon icon={faPlus} />
+                                        </div>
+                                    </div>
+
+                                    <h3 className="text-lg sm:text-xl md:text-2xl 2xl:text-[26px] font-light max-sm:leading-tight self-end mx-auto max-w-sm pb-10 text-white text-center">
+                                        {item.titulo}
+                                    </h3>
+                                </div>
+                            </Link>
                         </Reveal>
                     ))}
                 </div>
