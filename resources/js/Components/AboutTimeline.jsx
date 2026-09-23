9ad73b4916b9e6b5b10ca354d921c14ff372ef0b
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-09-23 10:35:24 -0300

    refactor: ajusta formatação e organização do código e slides breakpoints no componente AboutTimeline

--- resources/js/Components/AboutTimeline.jsx
+++ resources/js/Components/AboutTimeline.jsx
@@ -1,9 +1,9 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
-import { AboutTimelineSlides } from './AboutTimelineSlides';
+import { AboutTimelineSlides } from "./AboutTimelineSlides";
 
 export const AboutTimeline = ({ slides }) => {
     const prevButtonRef = useRef(null);
@@ -12,7 +12,7 @@ export const AboutTimeline = ({ slides }) => {
     const [activeIndex, setActiveIndex] = useState(0);
     const [slidesSwiperRef, setSlidesSwiperRef] = useState(null);
 
-    const slidesPerView = slides.length < 12 ? slides.length : 12;
+    // const slidesPerView = slides.length < 12 ? slides.length : 12;
 
     const handleSlideChange = (swiper) => {
         const newIndex = swiper.activeIndex;
@@ -28,7 +28,7 @@ export const AboutTimeline = ({ slides }) => {
 
     const handleChildSwiperInit = (childSwiper) => {
         setSlidesSwiperRef(childSwiper);
-        
+
         if (prevButtonRef.current && nextButtonRef.current) {
             childSwiper.params.navigation.prevEl = prevButtonRef.current;
             childSwiper.params.navigation.nextEl = nextButtonRef.current;
@@ -36,70 +36,94 @@ export const AboutTimeline = ({ slides }) => {
             childSwiper.navigation.update();
         }
     };
-    
+
     const handleSlideClick = (index) => {
         setActiveIndex(index);
         if (swiperRef.current) {
-            swiperRef.current.slideToLoop ? 
-                swiperRef.current.slideToLoop(index) : 
-                swiperRef.current.slideTo(index);
+            swiperRef.current.slideToLoop
+                ? swiperRef.current.slideToLoop(index)
+                : swiperRef.current.slideTo(index);
         }
-        
+
         if (slidesSwiperRef) {
-            slidesSwiperRef.slideToLoop ? 
-                slidesSwiperRef.slideToLoop(index) : 
-                slidesSwiperRef.slideTo(index);
+            slidesSwiperRef.slideToLoop
+                ? slidesSwiperRef.slideToLoop(index)
+                : slidesSwiperRef.slideTo(index);
         }
     };
 
     return (
-        <section className="pt-6 md:pt-12 2xl:pt-20">
+        <section className="pt-6 md:pt-12 2xl:pt-20 bg-black text-white">
             <div className="container max-w-large">
-                <h2 className="text-3xl md:text-4xl 2xl:text-[45px] text-center font-light uppercase tracking-wide leading-snug mb-8 md:mb-10 2xl:mb-20">Timeline</h2>
+                <h2 className="text-4xl md:text-5xl 2xl:text-6xl font-secondary  text-center tracking-wide leading-snug mb-8 md:mb-10 2xl:mb-20">
+                    Timeline
+                </h2>
 
-                <div className="relative border-b border-neutral-300 pb-4 px-10">
+                <div className="relative border-b border-neutral-600 pb-4 px-10">
                     <div className="max-w-[1350px] mx-auto">
                         <Swiper
-                            slidesPerView={slidesPerView}
+                            slidesPerView={2}
                             modules={[]}
                             loop={false}
+                            breakpoints={{
+                                640: {
+                                    slidesPerView: 4,
+                                    spaceBetween: 10,
+                                },
+                                768: {
+                                    slidesPerView: 6,
+                                    spaceBetween: 10,
+                                },
+                                1024: {
+                                    slidesPerView: Math.min(slides.length, 8),
+                                    spaceBetween: 10,
+                                },
+                                1280: {
+                                    slidesPerView: Math.min(slides.length, 12),
+                                    spaceBetween: 10,
+                                },
+                            }}
                             onBeforeInit={(swiper) => {
                                 swiperRef.current = swiper;
                             }}
                             onSlideChange={handleSlideChange}
                             initialSlide={0}
                         >
-                            {slides.map((slide, index) => 
+                            {slides.map((slide, index) => (
                                 <SwiperSlide key={index}>
                                     <div
                                         className="text-center w-24 mx-auto transition-all cursor-pointer hover:opacity-70"
                                         onClick={() => handleSlideClick(index)}
                                     >
-                                        <h5 className={`text-lg ${activeIndex === index ? 'font-bold' : 'font-light'} uppercase leading-snug p-4`}>{slide.ano}</h5>
+                                        <h5
+                                            className={`text-lg ${activeIndex === index ? "font-bold" : "font-light"} uppercase leading-snug p-4`}
+                                        >
+                                            {slide.ano}
+                                        </h5>
                                     </div>
                                 </SwiperSlide>
-                            )}
+                            ))}
                         </Swiper>
                     </div>
-                    
+
                     <button
                         ref={prevButtonRef}
                         className="[&:not(.swiper-button-lock)]:group w-16 h-16 absolute -top-1 -left-3 flex items-center justify-center [&.swiper-button-lock]:!flex [&.swiper-button-lock]:!opacity-50"
                     >
-                        <ArrowIcon className="rotate-180 fill-none stroke-black opacity-30 transition-all group-hover:opacity-100" />
+                        <ArrowIcon className="rotate-180 fill-none stroke-white transition-all group-hover:opacity-100" />
                     </button>
 
                     <button
                         ref={nextButtonRef}
                         className="[&:not(.swiper-button-lock)]:group w-16 h-16 absolute -top-1 -right-3 flex items-center justify-center [&.swiper-button-lock]:!flex [&.swiper-button-lock]:!opacity-50"
                     >
-                        <ArrowIcon className="fill-none stroke-black opacity-30 transition-all group-hover:opacity-100" />
+                        <ArrowIcon className="fill-none stroke-white transition-all group-hover:opacity-100" />
                     </button>
                 </div>
 
-                <AboutTimelineSlides 
-                    slides={slides} 
-                    activeIndex={activeIndex} 
+                <AboutTimelineSlides
+                    slides={slides}
+                    activeIndex={activeIndex}
                     onSwiperInit={handleChildSwiperInit}
                     onSlideChange={handleChildSlideChange}
                     prevButtonRef={prevButtonRef}
@@ -119,7 +143,14 @@ const ArrowIcon = ({ className }) => {
             viewBox="0 0 11.013 18"
             className={className}
         >
-            <path d="M0,0,9.4,8.05,0,16.107" transform="translate(0.946 0.946)" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="1.342"/>
+            <path
+                d="M0,0,9.4,8.05,0,16.107"
+                transform="translate(0.946 0.946)"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+                strokeWidth="1.342"
+            />
         </svg>
-    )
+    );
 };
